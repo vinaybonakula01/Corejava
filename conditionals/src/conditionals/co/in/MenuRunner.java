e9dddb48bb6134041085d13a1f839fcc1194b6d7
vinay bonakula <vinaybonakula5101@example.com>
2021-11-18 11:16:54 +0530

    conditionals

---
+++ conditionals/src/conditionals/co/in/MenuRunner.java
@@ -0,0 +1,45 @@
+package conditionals.co.in;
+
+import java.util.Scanner;
+
+public class MenuRunner {
+
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+		System.out.println("1.please enter number1");
+		int number1 = scanner.nextInt();
+		System.out.println("The First number u have enterd is --"+number1);
+		
+		System.out.println("2.please enter number2");
+		int number2 = scanner.nextInt();
+		System.out.println("The Second number u have enterd is -- "+number2);
+		
+		System.out.println("Availabe operations are:");
+		System.out.println("1.add");
+		System.out.println("2.sub");
+		System.out.println("3.multiply");
+		System.out.println("4.divide");
+		
+		System.out.println("select your operation");
+		int operation = scanner.nextInt();
+		System.out.println("u have selected operation no :1"+operation);
+		 
+		if(operation==1) {
+			System.out.println(number1+number2);
+		}
+		
+		if(operation==2) {
+			System.out.println(number1-number2);
+		}
+		if(operation==3) {
+			System.out.println(number1*number2);
+			
+		}
+		if(operation==4) {
+			System.out.println(number1%number2);
+		}
+		
+	
+		}
+
+        }
