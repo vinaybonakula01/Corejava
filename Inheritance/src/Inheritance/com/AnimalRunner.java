1e183679549bf3a99e6eddb7002613bd29e86716
vinay bonakula <85791047+vinaybonakula01@users.noreply.github.com>
2021-12-07 10:18:26 +0530

    Update AnimalRunner.java

--- Inheritance/src/Inheritance/com/AnimalRunner.java
+++ Inheritance/src/Inheritance/com/AnimalRunner.java
@@ -11,7 +11,7 @@ public class AnimalRunner {
 		List<Animal> dogimmutablelistAl =new ArrayList<>(dogimmutablelist);
 		List<Animal> catimmutablelist = List.of(new Cat("persian","white",30),new Cat("mainecoon","red",16),new Cat("bengalcat","multicolour",12));
 		List<Animal> catimmutablelistAl = new ArrayList<>(catimmutablelist);
-		Collections.sort(dogimmutablelistAl);
+	Collections.sort(dogimmutablelistAl);
         System.out.println(dogimmutablelistAl);
         Collections.sort(catimmutablelistAl);
         System.out.println(catimmutablelistAl);
